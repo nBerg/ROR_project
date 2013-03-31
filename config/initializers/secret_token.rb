9f6bb70837bbae1afefb3629e41eb5e697996a63
Nina Berg <nb2555@columbia.edu>
2013-03-31 02:58:44 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RORProject::Application.config.secret_token = 'e40debf632d2fbd4cc89a9bc86a65300a2be6207626f79c3c0a007a436637ec16bd09f227677f093d6a231b61ea15068f1fca8eed50f8354b15017d9514beb76'
