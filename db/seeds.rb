0775191ae6c310c3c8e6d3bed9dd9c74583ac0e8
Nina Berg <nb2555@columbia.edu>
2013-05-06 17:40:59 -0400

    Modified database seed task to work with email confirmations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Empty the database
-Mongoid.default_session.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
+Mongoid.purge!
+#Mongoid.default_session.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
 
 # Populate databse with some users and courses
 Rake::Task['import:import_users_csv'].invoke
@@ -18,4 +19,5 @@ Rake::Task['import:import_courses_csv'].invoke
 	c = Course.all.sample # find a random course
 	u = User.all.sample # find a random user
 	c.users << u unless c.users.include?(u) # create relationship, unless they are already related, to prevent duplicates
+  u.courses << c unless u.courses.include?(c)
 end
