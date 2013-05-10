42f061b4a35d20757da1d37fce7df399913875d3
Nina Berg <nb2555@columbia.edu>
2013-05-09 21:24:39 -0400

    Added markdown interpreter to lecture content

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -8,4 +8,19 @@ class Lecture
   field :num, type: Integer
   field :title, type: String
   field :content, type: String
+  field :content_html, type: String
+
+  before_save :generate_content_html
+
+  protected
+
+  	def generate_content_html
+  		return if content.blank?
+  		render_options = [:hard_wrap => true, :filter_html => true]
+  		options = [:no_intra_emphasis => true, :fenced_code_blocks => true, :autolink => true, :strikethrough => true, :space_after_header => true, :superscript => true, :underline => true]
+  		renderer = Redcarpet::Render::HTML.new(*render_options)
+  		markdown = Redcarpet::Markdown.new(renderer, *options)
+  		self.content_html = markdown.render(content)
+  	end
+
 end
